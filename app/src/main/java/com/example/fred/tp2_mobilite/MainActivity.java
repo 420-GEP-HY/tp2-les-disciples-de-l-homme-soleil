2d2706d2fe600c578353f867cf1868336f800dc9
Frédéric Blanchet-Angers <frederic.blanchet.angers@gmail.com>
2018-09-20 09:18:57 -0400

    Push du projet android studio

---
+++ app/src/main/java/com/example/fred/tp2_mobilite/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fred.tp2_mobilite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //guili guili
+        setContentView(R.layout.activity_main);
+    }
+}
